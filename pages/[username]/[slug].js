581cbbe32760dc7801e9698962a69670382d2f3d
Gustavo Barbosa Castilho <gustavobcx@gmail.com>
2022-03-10 00:27:51 -0300

    add likes / hearts

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,6 +1,8 @@
 import styles from '../../styles/Post.module.css';
 import PostContent from '../../components/PostContent';
 import Metatags from '../../components/Metatags';
+import HeartButton from '../../components/HeartButton';
+import AuthCheck from '../../components/AuthCheck';
 import { UserContext } from '../../lib/context';
 import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 
@@ -69,6 +71,16 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
+
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
 
         {currentUser?.uid === post.uid && (
           <Link href={`/admin/${post.slug}`}>
