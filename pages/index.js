ba7b18168f660b208097e594c58cfd3feaf6ef2e
Gustavo Barbosa Castilho <gustavobcx@gmail.com>
2022-03-09 23:45:28 -0300

    ssr-home-page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,71 @@
-import Link from 'next/link';
-
-import toast from 'react-hot-toast';
-
+import PostFeed from '../components/PostFeed';
 import Loader from '../components/Loader';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
-export default function Home() {
+import { useState } from 'react';
+
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'jeffd23' }
-      }}>Jeffs Profile</Link>
+    <main>
+      <div className="card card-info">
+        <h2>💡 Next.js + Firebase - The Full Course</h2>
+        <p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
+        <p>Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine optimized.</p>
+      </div>
+     
+      <PostFeed posts={posts} />
 
-      <Loader show />
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
 
-      <button onClick={() => toast.success('Hello World!')}>Hello World!</button>
+      <Loader show={loading} />
 
-    </div>
-  )
+      {postsEnd && 'You have reached the end!'}
+    </main>
+  );
 }
