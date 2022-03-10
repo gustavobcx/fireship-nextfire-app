ab3b1abf4f85977dc430bfa86ae3e0b722368ad8
Gustavo Barbosa Castilho <gustavobcx@gmail.com>
2022-03-09 22:57:37 -0300

    auth-google

--- pages/enter.js
+++ pages/enter.js
@@ -1,5 +1,51 @@
-export default function Page({}) {
-    return (
-        <>Hello World</>
-    )
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <>
+      <button className="btn-google" onClick={signInWithGoogle}>
+        <img src={"/google.png"} width="30px" /> Sign in with Google
+      </button>
+      <button onClick={() => auth.signInAnonymously()}>
+        Sign in Anonymously
+      </button>
+    </>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+// Username form
+function UsernameForm() {
+  return <></>;
 }
